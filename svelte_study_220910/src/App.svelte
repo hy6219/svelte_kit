01cdb772af1bfc3db33429fa4a4646079fd1df10
JSJeong <hy6219@naver.com>
2022-09-10 16:32:07 +0900

    feat: 컴포넌트

---
+++ svelte_study_220910/src/App.svelte
@@ -0,0 +1,37 @@
+<script>//스크립트 영역
+	export let name;
+	import Header from './ch03_components/header.svelte'
+	import Footer from './ch03_components/footer.svelte'; 
+	import Content from './ch03_components/content.svelte';
+</script>
+
+<!--markup 영역-->
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+<!--style 영역-->
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+<Header/>
+<Content/>
+<Footer/>
---
+++ svelte_study_220910/src/ch03_components/content.svelte
@@ -0,0 +1,5 @@
+<script> //스크립트 영역
+    import SubContent from './SubContent.svelte'
+</script>
+<h2>Content Space</h2>
+<SubContent/>
