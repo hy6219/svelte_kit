eb657fa95640079d94bf69b70709862c6c1bc1ac
JSJeong <hy6219@naver.com>
2022-09-10 16:49:16 +0900

    feat: 스벨트 state 컴포넌트

--- svelte_study_220910/src/App.svelte
+++ svelte_study_220910/src/App.svelte
@@ -3,12 +3,24 @@
 	import Header from './ch03_components/header.svelte'
 	import Footer from './ch03_components/footer.svelte'; 
 	import Content from './ch03_components/content.svelte';
+
+	//상태(state)값
+	let count = 0;
+
+	const handleClick = ()=>{
+		count++;
+	};
+
 </script>
 
 <!--markup 영역-->
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<button on:click={handleClick}>
+		클릭 수 {count}
+	</button>
 </main>
 <!--style 영역-->
 <style>
